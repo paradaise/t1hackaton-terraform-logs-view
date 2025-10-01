713023ca60f1a63bc66ffcd7a96809f0e3845d5c
Bogdan_Chudopalov <tchudopalov.bogdan@yandex.ru>
2025-10-01 10:24:12 +0300

    Добавил docker-compose файл и внес изменения в vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,34 +5,36 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 5173,
     proxy: {
       '/upload': {
-        target: 'http://127.0.1.1:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
         secure: false,
       },
       '/logs': {
-        target: 'http://127.0.1.1:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
         secure: false,
       },
       '/metrics': {
-        target: 'http://127.0.1.1:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
         secure: false,
       },
       '/timeline': {
-        target: 'http://127.0.1.1:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
         secure: false,
       },
       '/groups': {
-        target: 'http://127.0.1.1:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
         secure: false,
       },
       '/export': {
-        target: 'http://127.0.1.1:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
         secure: false,
       },
